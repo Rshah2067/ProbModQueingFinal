75784befab8e45e44a009177d6849e160fa33595
Rohan Shah <rohanshah2067@gmail.com>
2025-12-10 01:34:16 -0500

    Fixed model bug and plot

--- m1model.py
+++ m1model.py
@@ -1,9 +1,11 @@
 import numpy as np
 import numpy.random as rv
 import tqdm
+import matplotlib.pyplot as plt
 
 
-service_state = false
+
+service_state = False
 queue_size = 0
 process_time = 0
 completion_time = 0
@@ -15,16 +17,31 @@ lmb = 0.1 #arrival rate (arrivals/step)
 mu = 2 #service rate (time to serve one)
 
 
-for i in tqdm(range(1,num_steps)):
+for i in range(1,num_steps):
   queue_size += rv.exponential(lmb)
   queue_plot.append(queue_size)
-  if service_state == false:
+  if service_state == False:
     process_time = rv.exponential(mu)
     completion_time = i + process_time
-    service_state = true
+    service_state = True
   else:
     if completion_time > i:
       pass
     else:
-      service_state = false
+      service_state = False
+    
+timesteps = range(len(queue_plot))
 
+plt.figure(figsize=(10, 6))
+plt.scatter(timesteps, queue_plot,s = 1, marker='.', linestyle='-', color='blue')
+plt.title('Queue Length Over Time')
+plt.xlabel('Timestep')
+plt.ylabel('Queue Length')
+plt.grid(True)
+xticks = []
+for step in timesteps:
+  if step %50 ==0:
+    xticks.append(step)
+
+plt.xticks(xticks) # Ensure all timesteps are marked on the x-axis
+plt.show()
--- visualization.py
+++ visualization.py
@@ -3,7 +3,6 @@ import matplotlib.pyplot as plt
 # Ensure queue_plot is defined. Assuming it's already available from previous cells.
 # In the provided notebook state, the variable is named 'queue_plot'.
 # If it were 'queue_size' as previously inferred, please adjust the variable name.
-def plot (queue_plot)
     timesteps = range(len(queue_plot))
 
     plt.figure(figsize=(10, 6))
